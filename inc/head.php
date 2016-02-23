7631ea3d64434d0604debba40df0407e7c98b7b2
Giuseppe Naponiello <beppenapo@arc-team.com>
2016-02-23 12:19:03 +0100

    1602231218 aggiunto link al menù principale e sistemato il layout

--- inc/head.php
+++ inc/head.php
@@ -9,11 +9,11 @@ if (isset($_SESSION['id_user'])&&$_SESSION['id_user']>0){require("inc/sessione.p
 ?>
 <div class="wrap headWrap">
     <div id="headLogo" class="headDiv"></div>
-    <div id="headTitle" class="headDiv bianco"> 
+    <div id="headTitle" class="headDiv bianco">
         <h1>ARCHIVIO ICONOGRAFICO DEI PAESAGGI DI COMUNITÀ</h1>
         <h2>Comunità Alta Valsugana e Bersntol<br/>Tolgamoa'schòft Hoa Valzegu' ont Bersntol</h2>
     </div>
-    <div id="headNav" class="headDiv"> 
+    <div id="headNav" class="headDiv">
         <div id="record" class="bianco">Ad oggi l'archivio contiene <?php echo $tot; ?> record</div>
         <nav class="headDiv">
             <ul>
@@ -22,6 +22,7 @@ if (isset($_SESSION['id_user'])&&$_SESSION['id_user']>0){require("inc/sessione.p
                 <li><a href="#" id="catalogo" class="mainLink prevent" title="Vai alla pagina del catalogo">CATALOGO</a></li>
                 <li><a href="#" id="collezioni" class="mainLink prevent" title="Vai alla pagina delle collezioni">COLLEZIONI</a></li>
                 <li><a href="#" id="contatti" class="mainLink prevent" title="Vai alla pagina dei contatti">CONTATTI</a></li>
+                <li><a href="#" id="collabora" class="mainLink prevent" title="Collabora con l&#39;archivio">COLLABORA</a></li>
                 <li><a href="#" id="istruzioni" class="mainLink prevent" title="Vai alla pagina delle istruzioni">ISTRUZIONI</a></li>
                 <li><a href="#" id="credits" class="mainLink prevent" title="Vai alla pagina dei credits">CREDITS</a></li>
             </ul>
