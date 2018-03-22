6d492f8bf5128a8c329c5549be67e1d8605887b9
Giuseppe Naponiello <beppenapo@arc-team.com>
2018-03-22 15:57:10 +0100

    rebase db file

--- inc/db.php
+++ inc/db.php
@@ -3,6 +3,5 @@ $dbhost =  getenv('PDCH');
 $dbusername =  getenv('PDCU');
 $password=  getenv('PDCP');
 $database_name =  getenv('PDCD');
-
 $connection = pg_connect("host=".$dbhost." user=".$dbusername." password=".$password." dbname=$database_name") or die ("Impossibile connettersi al server");
 ?>
